62a510e6ba011711e1e629beda91b2dbd1a07f05
gabrieleduardofleck <48869659+gabrieleduardofleck@users.noreply.github.com>
2020-02-22 12:08:43 -0300

    Birl

---
+++ fun-projects/birl-simulator/birl.js
@@ -0,0 +1,28 @@
+//birl
+
+function Birl() {
+	
+	document.getElementById("birl").play();
+
+}
+
+
+function Wrah() {
+	
+	document.getElementById("wrah").play();
+
+}
+
+
+function Show() {
+	
+	document.getElementById("show").play();
+
+}
+
+function Bora() {
+	
+	document.getElementById("bora").play();
+
+}
+
